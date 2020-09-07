ed4675fd75ec9e7f646dde08092474b1cb61247b
Sofia Pellegrini <amapetrice@gmail.com>
2020-09-06 22:10:11 -0300

    Revisa funcion flota

--- flota.py
+++ flota.py
@@ -3,44 +3,28 @@
 
 
 def leerArchivo():
-	tupla_barcos = []
+	tuplas_barcos = []
 	tupla_x = []
 	tupla_y = []
 	count= 0
 	f = open('Salida.txt', 'r')
 	barcos = f.readlines()
-	print(barcos)
 	for barco in barcos:
-		# ~ barco = list(string)
-		# ~ print(barco)
 		for i in range(0, len(barco)-1):
 			if barco[i] in ['A','B','C','D','E','F','G','H','I','J']:
-				# ~ print(barco[i])
 				tupla_x.append(barco[i])
 			if barco[i] in ['1','2','3','4','5','6','7','8','9']:
-				if barco[int(i)] == '0':
-					break
-				elif barco[int(i)+ 1] == '0':
-					# ~ print('10')
+				if barco[int(i)+ 1] == '0':
 					tupla_y.append(10)
 				else:
-					# ~ print(barco[i])
 					tupla_y.append(int(barco[i]))
 
-	# [(A,10),(B,10),(C,10),(D,10)]
-    #      ^
-	print("NUEVA LINEA")
-	print(tupla_x)
-	print(tupla_y)
-
-
-	for i in range (0,len(tupla_x)):
+	for i in range(len(tupla_x)):
 		s = (tupla_x[i],tupla_y[i])
-		# ~ print(s)
-		tupla_barcos.append(s)
-	return tupla_barcos
-				
-				
+		tuplas_barcos.append(s)
+	return tuplas_barcos
+
+
 def tableroCorrecto(tupla):
 	tableroVacio = [[ 'v' for i in range(10) ] for j in range(10) ] #cambiarle el nombre a la variable
 	letritas = {'A': 0, 'B': 1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9}
